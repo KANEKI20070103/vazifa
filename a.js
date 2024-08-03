8b7e6ec80ba43a09e20fbdd953f6fa33e46491f7
KANEKI20070103 <nurlievaadolat4@gmail.com>
2024-08-03 11:01:22 +0500

    vazifa

---
+++ a.js
@@ -0,0 +1,85 @@
+// a
+
+// const product = {
+//     nomi: "Kompyuter",
+//     narxi: 6512
+// }
+//
+// console.log(`Maxsulot narxi : ${product.narxi} cent`)
+
+// b
+
+
+// const product = {
+//     nomi: "Kompyuter",
+//     narxi: 6512
+// }
+//
+// product.narxi+=500
+//
+// console.log(product.narxi)
+
+// const product = {
+//     nomi: "Kompyuter",
+//     narxi: 6512
+// }
+//
+// product['Yetkazib berish vaqti'] = " 4 kun "
+// console.log(product)
+
+
+// c
+
+//
+// const product = {
+//     nomi: "Kompyuter",
+//     narxi: 6512
+// }
+// const product2 = {
+//     nomi: "Noutbuk",
+//     narxi: 4520
+//
+// }
+//
+// function  compareProducts(product1, product2) {
+//     if (product1.narxi > product2.narxi) {
+//         console.log(product1)
+//     }
+//     else {
+//         console.log(product2)
+//     }
+// }
+// compareProducts(product,product2)
+
+
+// d
+
+// const product = {
+//     nomi: "Kompyuter",
+//     narxi: 6512
+// }
+// const product2 = {
+//     nomi: "Noutbuk",
+//     narxi: 4520
+//
+// }
+//
+// function  compareProducts(product, product2) {
+//     if (product.narxi === product2.narxi) {
+//         console.log(true)
+//     }
+//     else {
+//         console.log(false)
+//     }
+// }
+// compareProducts(product,product2)
+
+
+
+//
+// const originalString = 'Good Morning';
+// const lowerCaseString = originalString.toLocaleLowerCase();
+//
+// console.log(lowerCaseString);
+
+
